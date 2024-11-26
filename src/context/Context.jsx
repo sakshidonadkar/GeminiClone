5edadecc41abab50fc9f67ae3972d9d6e392b2d5
sakshidonadkar <85394658+sakshidonadkar@users.noreply.github.com>
2024-11-26 13:12:59 +0530

    Context API Integration

---
+++ src/context/Context.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react"
+import run from "../config/gemini"
+
+export const Context = createContext()
+
+const ContextProvider = (props) => {
+
+    const onSent = async (prompt) => {
+        await run(prompt)
+    }
+ onSent("Who is mother of dragons in Game of Throne?")
+
+   
+
+  const contextValue = {
+  
+  }
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  )
+}
+
+export default ContextProvider
